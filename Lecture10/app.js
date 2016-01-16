5b46aa705680e0744c36e39e9029e6871a1cb595
Timurtek Bizel <timurbizel88@gmail.com>
2016-01-16 08:08:14 -0800

    first commit

---
+++ Lecture10/app.js
@@ -0,0 +1,8 @@
+var a = 'Hello World!';
+
+function b(){
+  console.log('Called B!');
+}
+
+b();
+console.log(a);
---
+++ Lecture11/app.js
@@ -0,0 +1,8 @@
+var a = "Hello";
+console.log(a);
+
+if(a === undefined){
+  console.log('a is undefined');
+}else{
+  console.log('a is defined');
+}
